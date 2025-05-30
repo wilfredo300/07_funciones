e19f9f3bf6eff76f2d8d7e9f27881e29dbeb9efd
wilfredo300 <junior.val6@gmail.com>
2025-05-30 10:47:52 -0500

    Creacion de ejercicio 5: Determinar si dos numeros son amigos

---
+++ ejercicio5.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int sumdiv(int n){
+    int i,s(0);
+        for (i=1;i<n;i++){
+            if(n%i==0){
+                s=s+i;
+            }
+        }
+    return s;    
+}
+
+int main(){
+    int n1,n2,a,b;
+    cin>>n1>>n2;
+    a=sumdiv(n1);
+    b=sumdiv(n2);
+    if(a==n2 &&b==n1){
+        cout<<"Los numeros son amigos";
+    }else{
+        cout<<"Los numeros no son amigos";
+    }
+   
+    return 0;
+
+}
