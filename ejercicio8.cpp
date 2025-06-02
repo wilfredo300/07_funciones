38b3ed81e82420b0952bb23dadda652c586f191c
wilfredo300 <junior.val6@gmail.com>
2025-06-02 14:35:16 -0500

    Se modifico ejercicio8.cpp para merjorar su apariencia

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -5,11 +5,18 @@ int main(){
     int i,n;
     float s;
     s=0;
-    cout<<"Ingresa el valor de n:\n";
+     cout<<  "------------------------------------------------------------\n";
+    cout<<  "\n       Programa para calcular la suma de la serie: \n\n"
+            "                     1!    2!    3!        n!\n"
+            "                S = --- + --- + --- +... +---  \n"
+            "                     2     4     6        2n!\n"<<endl;
+    cout<<  "------------------------------------------------------------\n";
+    cout<<"Ingresa el valor de n: ";
     cin>>n;
     for(i=1;i<=n;i++){
         s=s+(factorial(i)/(2*i));
     }
+    cout<<"El valor de S es: ";
     cout<<s;
     return 0;   
 }
